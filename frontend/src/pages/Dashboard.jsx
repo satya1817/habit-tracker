75bb428367aedf9cbae819beafc241d0304dcd74
satya1817 <thekhushi2008@gmail.com>
2026-06-16 11:05:19 +0530

    Created dashboard and habit service

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+
 function Dashboard() {
-  return <h1>Dashboard Page</h1>;
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(title);
+  };
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter Habit"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <button type="submit">
+          Add Habit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Dashboard;
---
+++ frontend/src/services/habitService.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api/habits";
+
+const getToken = () => {
+  return localStorage.getItem("token");
+};
+
+export const createHabit = async (habitData) => {
+  const response = await axios.post(
+    API_URL,
+    habitData,
+    {
+      headers: {
+        Authorization: `Bearer ${getToken()}`,
+      },
+    }
+  );
+
+  return response.data;
+};
+
+export const getHabits = async () => {
+  const response = await axios.get(API_URL, {
+    headers: {
+      Authorization: `Bearer ${getToken()}`,
+    },
+  });
+
+  return response.data;
+};
