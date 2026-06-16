6fb7d6183a0916bd05cda9df2d5036bdbd63047e
satya1817 <thekhushi2008@gmail.com>
2026-06-16 10:55:26 +0530

    Created login page UI

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log({
+      email,
+      password,
+    });
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
